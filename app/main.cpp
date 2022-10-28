ca81863d7fef79f0716aa3426be13b4d40e992bc
Filip Lončar <filoncar@vsite.hr>
2022-10-28 19:59:14 +0200

    main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -1,19 +1,39 @@
 #include "app.h"
+#include <stdio.h>
+#include<iostream>
 
-// read number of results
-// read all student data
-// read grade and output grade statistics
-// read letter and output letter statistics
-int main()
-{
-	/*int n; std::cin >> n;
-	results r(n);
-	for (int i = 0; i < n; ++i) {
-		student s;
-		std::cin >> s.name >> s.grade;
-		r.add(s)
-	}*/
 
-	vsite::oop::v3::results(5);
 
-}
+
+using namespace::vsite::oop::v3;
+using namespace::std;
+	int main(){
+		
+		// read number of results
+		cout << "Unesite broj za koliko studenata zelite napraviri ucitavanje: ";
+		unsigned int input = 0;
+		cin >> input;
+
+		results r(input);
+		// read all student data
+		for (auto i = 0; i < input; ++i) {
+			vsite::oop::v3::student student;
+
+			cout << "Ime i ocjena: ";
+			cin >> student.name >> student.grade;
+			r.add(student);
+		}
+
+		// read grade and output grade statistics
+		cout << "Ocjena za koju zelite statistiku: ";
+		unsigned int grade = 0;
+		cin >> grade;
+		cout << r.has_grade(grade) << endl;
+		
+		// read letter and output letter statistics
+		cout << "Unesite prvo slovo studenta: ";
+		char chr = NULL;
+		cin >> chr;
+		cout << r.starts_with_letter(chr) << endl;
+
+	}
