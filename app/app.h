644733688139e190f986551900f0d6dd171e16a8
MislavBucar <mislavbucar@gmail.com>
2022-11-14 12:47:38 +0100

    vsite vjezba 3

--- app/app.h
+++ app/app.h
@@ -1,7 +1,29 @@
 #pragma once
 #include <string>
+#include <cctype>
 
-namespace vsite::oop::v3 
+namespace vsite::oop::v3
 {
+	class student {
 
+	public:
+		student() : grade(0), name("") {};
+		student(std::string name, int grade) : grade(grade), name(name) {};
+		std::string name;
+		int grade;
+	};
+
+	class results {
+		student* studentArr;
+		int mLength = 0;
+		int i = 0;
+
+	public:
+		results(int length);
+		~results() { delete[] studentArr; };
+
+		void add(student myStudent);
+		unsigned int has_grade(int n);
+		unsigned int starts_with_letter(char l);
+	};
 }
--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,31 @@
 #include "app.h"
+#include <iostream>
+using namespace vsite::oop::v3;
 
 // read number of results
 // read all student data
 // read grade and output grade statistics
 // read letter and output letter statistics
+
 int main()
 {
+	student s{ "pero", 2 };
+
+	std::cout << s.name.c_str() << std::endl;
+	std::cout << s.grade << std::endl;
+	std::cout << std::endl;
+
+	results r(8);
+	r.add({ "pero", 2 });
+	r.add({ "marko", 3 });
+	r.add({ "petra", 3 });
+	r.add({ "ivo",	1 });
+	r.add({ "drazen", 5 });
+	r.add({ "paula", 5 });
+	r.add({ "patrik", 3 });
+	r.add({ "ivana", 2 });
+
+	std::cout << "grades: " << r.has_grade(3) << std::endl;
+	std::cout << "names: " << r.starts_with_letter('p') << std::endl;
 }
+
