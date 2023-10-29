b7f671350407307e4922e300598b717232390f0c
Luka Blagojević <lublagoj@vsite.hr>
2023-10-29 20:02:40 +0100

    riješena vježba

--- app/app.h
+++ app/app.h
@@ -1,43 +1,43 @@
-#pragma once
-#include <string>
+    #pragma once
+    #include <string>
 
-namespace vsite::oop::v3
-{
-    struct student {
-        std::string name;
-        int grade = 0;
-    };
+    namespace vsite::oop::v3
+    {
+        struct student {
+            std::string name;
+            int grade = 0;
+        };
 
-    class results {
-        int brojac = 0;
-        int pos = 0;
-        student* sNiz = nullptr;
+        class results {
+            int brojac = 0;
+            int pos = 0;
+            student* sNiz = nullptr;
 
-    public:
-        results(int brojac = 0);
-        void add(const student& s);
-        unsigned has_grade(int g);
-        unsigned starts_with_letter(char c);
-        ~results();
-    };
+        public:
+            results(int brojac = 0);
+            void add(const student& s);
+            unsigned has_grade(int g);
+            unsigned starts_with_letter(char c);
+            ~results();
+        };
 
-    class array {
-        unsigned _size;
-        double* nizArr;
+        class array {
+            unsigned _size;
+            double* nizArr;
 
-    public:
-        array(unsigned s = 0, double value = 0.0);
-        array(const array& source);
-        array(array&& source);
-        ~array();
+        public:
+            array(unsigned s = 0, double value = 0.0);
+            array(const array& source);
+            array(array&& source);
+            ~array();
 
-        unsigned size() {
-            return _size;
-        }
+            unsigned size() {
+                return _size;
+            }
 
-        double at(int i);
+            double at(int i);
 
-        void push_back(double value);
+            void push_back(double value);
 
-    };
-}
+        };
+    }
