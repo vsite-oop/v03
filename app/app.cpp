90abc1c1e4ededf01651751afbc75489365f24be
Tomislav Pavic <t.pavic@outlook.com>
2023-10-29 16:35:43 +0100

    vj 3 solved

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,91 @@
 
 namespace vsite::oop::v3
 {
+	results::results(int c) {
+		count = c;
+		studNiz = new student[count];
+	}
 
+	void results::add(const student& s) {
+		studNiz[pos] = s;
+		pos++;
+	}
+
+	unsigned results::has_grade(int g) {
+		int studCount = 0;
+
+		for (int i = 0; i < count; i++) {
+			if (studNiz[i].grade == g) {
+				studCount++;
+			}
+		}
+		return studCount;
+	}
+
+	unsigned results::starts_with_letter(char c) {
+
+		int studCount = 0;
+
+		for (int i = 0; i < count; i++) {
+			if (tolower(studNiz[i].name[0]) == tolower(c)) {
+				studCount++;
+			}
+		}
+		return studCount;
+	}
+	results::~results()
+	{
+		delete[] studNiz;
+	}
+
+	array::array(unsigned s, double value) {
+
+		_size = s;
+		nizArr = new double[s];
+
+		for (int i = 0; i < s; i++) {
+			nizArr[i] = value;
+		}
+	}
+
+	array::array(const array& s)
+	{
+		_size = s._size;
+		nizArr = new double[_size];
+
+		for (int i = 0; i < _size; i++) {
+			nizArr[i] = s.nizArr[i];
+		}
+	}
+
+	array::array(array&& s) : _size(s._size), nizArr(s.nizArr) {
+		s._size = 0;
+		s.nizArr = nullptr;
+	}
+
+	double array::at(int i) {
+		if (i < 0 || i >= _size) {
+			return 0.;
+		}
+
+		return nizArr[i];
+	}
+
+	void array::push_back(double val) {
+		int tempSize = _size + 1;
+		double* tempNiz = new double[tempSize];
+
+		for (int i = 0; i < tempSize - 1; i++) {
+			tempNiz[i] = nizArr[i];
+		}
+		tempNiz[tempSize - 1] = val;
+
+		delete[] nizArr;
+		nizArr = tempNiz;
+		_size = tempSize;
+	}
+
+	array::~array() {
+		delete[] nizArr;
+	}
 }
--- app/main.cpp
+++ app/main.cpp
@@ -1,4 +1,12 @@
 #include "app.h"
+#include <iostream>
+
+using vsite::oop::v3::student;
+using vsite::oop::v3::array;
+using vsite::oop::v3::results;
+using std::cout;
+using std::cin;
+
 
 // read number of results
 // read all student data
@@ -6,4 +14,29 @@
 // read letter and output letter statistics
 int main()
 {
+    student stud;
+    int studCount;
+
+    cout << "Unesite broj studenata: ";
+    cin >> studCount;
+
+    results studData(studCount);
+
+    for (int i = 0; i < studCount; i++) {
+        cout << "Unesite ime i ocjenu: ";
+        cin >> stud.name >> stud.grade;
+
+        studData.add(stud);
+    }
+
+    int statOcjena;
+    cout << "Unesite ocjenu za pretraziti: ";
+    cin >> statOcjena;
+    cout << "Broj studenata koji ima trazenu ocjenu " << statOcjena << " iznosi:  " << studData.has_grade(statOcjena) << std::endl;
+
+    char statSlovo;
+    cout << "Unesite slovo: ";
+    cin >> statSlovo;
+    cout << "Broj studenata koji pocinju sa slovom: " << statSlovo << " ima: " << studData.starts_with_letter(statSlovo) << std::endl;
 }
+
