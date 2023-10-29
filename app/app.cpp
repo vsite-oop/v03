fd1b9de86ad82bd7092662c4671bd8467561b5b8
Luka Blagojević <lublagoj@vsite.hr>
2023-10-29 19:54:36 +0100

    luka blagojevic

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,91 @@
 
 namespace vsite::oop::v3
 {
+    results::results(int c) {
+        brojac = c;
+        sNiz = new student[brojac];
+    }
 
+    void results::add(const student& s) {
+        sNiz[pos] = s;
+        pos++;
+    }
+
+    unsigned results::has_grade(int g) {
+        int sBrojac = 0;
+
+        for (int i = 0; i < brojac; i++) {
+            if (sNiz[i].grade == g) {
+                sBrojac++;
+            }
+        }
+        return sBrojac;
+    }
+
+    unsigned results::starts_with_letter(char c) {
+
+        int sBrojac = 0;
+
+        for (int i = 0; i < brojac; i++) {
+            if (tolower(sNiz[i].name[0]) == tolower(c)) {
+                sBrojac++;
+            }
+        }
+        return sBrojac;
+    }
+    results::~results()
+    {
+        delete[] sNiz;
+    }
+
+    array::array(unsigned s, double value) {
+
+        _size = s;
+        nizArr = new double[s];
+
+        for (int i = 0; i < s; i++) {
+            nizArr[i] = value;
+        }
+    }
+
+    array::array(const array& s)
+    {
+        _size = s._size;
+        nizArr = new double[_size];
+
+        for (int i = 0; i < _size; i++) {
+            nizArr[i] = s.nizArr[i];
+        }
+    }
+
+    array::array(array&& s) : _size(s._size), nizArr(s.nizArr) {
+        s._size = 0;
+        s.nizArr = nullptr;
+    }
+
+    double array::at(int i) {
+        if (i < 0 || i >= _size) {
+            return 0.;
+        }
+
+        return nizArr[i];
+    }
+
+    void array::push_back(double val) {
+        int tempSize = _size + 1;
+        double* tempNiz = new double[tempSize];
+
+        for (int i = 0; i < tempSize - 1; i++) {
+            tempNiz[i] = nizArr[i];
+        }
+        tempNiz[tempSize - 1] = val;
+
+        delete[] nizArr;
+        nizArr = tempNiz;
+        _size = tempSize;
+    }
+
+    array::~array() {
+        delete[] nizArr;
+    }
 }
--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,34 @@
 #include "app.h"
+#include <iostream>
 
-// read number of results
-// read all student data
-// read grade and output grade statistics
-// read letter and output letter statistics
+using vsite::oop::v3::student;
+using vsite::oop::v3::array;
+using vsite::oop::v3::results;
+
 int main()
 {
+    student s;
+    int sBrojac;
+
+    std::cout << "Unesite broj studenata: ";
+    std::cin >> sBrojac;
+
+    results sData(sBrojac);
+
+    for (int i = 0; i < sBrojac; i++) {
+        std::cout << "Unesite ime i ocjenu: ";
+        std::cin >> s.name >> s.grade;
+
+        sData.add(s);
+    }
+
+    int statO;
+    std::cout << "Unesite ocjenu koju zelite: ";
+    std::cin >> statO;
+    std::cout << "Broj studenata :'" << statO << "' : " << sData.has_grade(statO) << std::endl;
+
+    char statS;
+    std::cout << "Unesite slovo: ";
+    std::cin >> statS;
+    std::cout << "Broj studenata sa pocetnim slovom '" << statS << "' : " << sData.starts_with_letter(statS) << std::endl;
 }
